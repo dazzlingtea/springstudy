8eb52bfdf64a31def05e401cc5926d3f17fb6329
dazzlingtea <kitsnr08@gmail.com>
2024-05-14 15:59:35 +0900

    오타 수정

--- src/main/java/com/study/springstudy/core/chap03/HotelTest.java
+++ src/main/java/com/study/springstudy/core/chap03/HotelTest.java
@@ -1,13 +1,13 @@
-package com.study.springstudy.core.chap03;
-
-import org.junit.Test;
-
-public class HotelTest {
-
-    @Test
-    public void hotelTest() {
-//        Hotel hotel = new Hotel();
-//        hotel.inform();
-    }
-
-}
+//package com.study.springstudy.core.chap03;
+//
+////import org.junit.Test;
+//
+//public class HotelTest {
+//
+//    @Test
+//    public void hotelTest() {
+////        Hotel hotel = new Hotel();
+////        hotel.inform();
+//    }
+//
+//}
