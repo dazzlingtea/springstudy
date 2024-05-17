c5a07b86c74808cd1e2c6cbdace513a75a49f312
dazzlingtea <kitsnr08@gmail.com>
2024-05-17 17:19:07 +0900

    Model 클래스명 변경

--- src/main/java/com/study/springstudy/webservlet/chap02/v4/controller/SaveController.java
+++ src/main/java/com/study/springstudy/webservlet/chap02/v4/controller/SaveController.java
@@ -1,7 +1,7 @@
 package com.study.springstudy.webservlet.chap02.v4.controller;
 
 import com.study.springstudy.webservlet.MemberMemoryRepo;
-import com.study.springstudy.webservlet.Model;
+import com.study.springstudy.webservlet.MyModel;
 import com.study.springstudy.webservlet.entity.Member;
 
 import java.util.Map;
@@ -10,7 +10,7 @@ public class SaveController implements ControllerV4 {
     private MemberMemoryRepo repo = MemberMemoryRepo.getInstance();
 
     @Override
-    public String process(Map<String, String> paramMap, Model model) {
+    public String process(Map<String, String> paramMap, MyModel myModel) {
         String userName = paramMap.get("userName");
         String account = paramMap.get("account");
         String password = paramMap.get("password");
