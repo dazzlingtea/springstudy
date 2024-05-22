635e8ce86773516529e215d9cb38906bc27b8eae
dazzlingtea <kitsnr08@gmail.com>
2024-05-22 11:40:31 +0900

    페이징 기초 - prev, next

--- src/main/java/com/study/springstudy/springmvc/chap04/common/PageMaker.java
+++ src/main/java/com/study/springstudy/springmvc/chap04/common/PageMaker.java
@@ -15,6 +15,9 @@ public class PageMaker {
     // 페이지 시작번호와 끝번호
     private int begin, end;
 
+    // 이전, 다음버튼 활성화 여부
+    private boolean prev, next;
+
     // 총 게시물 수
     private int totalCount;
 
@@ -74,7 +77,11 @@ public class PageMaker {
         if(finalPage < this.end) {
             this.end = finalPage;
         }
+        // 4. 이전버튼 활성화 여부
+        this.prev = begin != 1;
 
+        // 5. 다음버튼 활성화 여부
+        this.next = end < finalPage;
     }
 
 }
