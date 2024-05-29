a792aa7a1a18aa92cf22c6421c566a2a601823ad
dazzlingtea <kitsnr08@gmail.com>
2024-05-29 14:44:50 +0900

    회원관리 1 - DB, entity, xml

--- src/main/resources/dddd.sql
+++ src/main/resources/dddd.sql
@@ -117,3 +117,15 @@ where board_no = 101;
 
 select * from tbl_reply
 where board_no = 100;
+
+
+-- 회원 관리 테이블
+CREATE TABLE tbl_member (
+         account VARCHAR(50),
+         password VARCHAR(150) NOT NULL,
+         name VARCHAR(50) NOT NULL,
+         email VARCHAR(100) NOT NULL UNIQUE,
+         auth VARCHAR(20) DEFAULT 'COMMON',
+         reg_date DATETIME DEFAULT current_timestamp,
+         CONSTRAINT pk_member PRIMARY KEY (account)
+);
