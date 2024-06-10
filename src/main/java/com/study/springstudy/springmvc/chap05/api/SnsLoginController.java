c7b76c547599c494b2e66078366fa7664f2aab94
dazzlingtea <kitsnr08@gmail.com>
2024-06-10 11:11:31 +0900

    # 카카오 로그인 2. 토큰, 사용자정보

--- src/main/java/com/study/springstudy/springmvc/chap05/api/SnsLoginController.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/api/SnsLoginController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
+
 @Controller
 @Slf4j
 @RequiredArgsConstructor
@@ -36,8 +38,14 @@ public class SnsLoginController {
     public String kakaoCode(String code) {
         log.info("카카오 인가코드 발급 - {}", code);
 
-        // 인증 액세스 토큰 발급 요청
+        // 토큰 발급에 필요한 파라미터 만들기
+        HashMap<String, Object> requestParams = new HashMap<>();
+        requestParams.put("appKey", appKey);
+        requestParams.put("redirect", redirectUri);
+        requestParams.put("code", code);
 
+        // 인증 액세스 토큰 발급 요청
+        snsLoginService.kakaoLogin(requestParams);
 
         return "";
     }
